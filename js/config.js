b59218f2d373c445fa074b79df8de616b8111d4b
Shreyojit <85859037+Shreyojit@users.noreply.github.com>
2025-01-07 09:56:22 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,6 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'ap-south-1_vFlojEdh0', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '75crfcrtu6lhhhl94cnbq5e1pj', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        client_secret: 'secret123123',
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
